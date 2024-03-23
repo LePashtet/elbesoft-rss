2cec7adda186960b3cefc06bad9b7bb0aeda8eb2
LyoLik1 <lyosha.neznakomov@gmail.com>
2024-03-23 01:52:26 +0100

    feat: newsletter rss

--- newsletterRss/index.js
+++ newsletterRss/index.js
@@ -7,13 +7,13 @@ async function fetchRSSData(rssUrl) {
 }
 
 export async function getFilteredRSSData(rssUrl, newsTopic) {
-  try {
-    const today = new Date().toDateString();
-    const newsTheme = newsTopic;
 
-    const xmlData = await fetchRSSData(rssUrl);
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async(resolve, reject) => {
+      const today = new Date().toDateString();
+      const newsTheme=newsTopic.toLowerCase()
+      const xmlData =await fetchRSSData(rssUrl);
+      
       xml2js.parseString(xmlData, (err, result) => {
         if (err) {
           console.error("Error parsing XML:", err);
@@ -23,29 +23,27 @@ export async function getFilteredRSSData(rssUrl, newsTopic) {
         const items = result.rss.channel[0].item;
 
         const filteredItems = items.filter((item) => {
-          const pubDate = new Date(item.pubDate[0]).toDateString();
+          const publishDate = new Date(item.pubDate[0]).toDateString();
           const newsTitle = item.title[0].toLowerCase();
           const newsDescription = item.description[0].toLowerCase();
 
-          return (
-            pubDate === today &&
-            (newsDescription.includes(newsTheme) ||
-              newsTitle.includes(newsTheme))
-          );
+          const isToday = publishDate === today
+          const isIncludeTheme = newsDescription.includes(newsTheme) ||
+          newsTitle.includes(newsTheme)
+
+          return isToday && isIncludeTheme
         });
 
-        const simplifiedItems = filteredItems.map((item) => ({
+        const simplifiedItems = filteredItems.map((item) => {
+          const media = item["media:content"] ? item["media:content"][0].$.url : null;
+          return{
           title: item.title[0],
           description: item.description[0],
           link: item.link[0],
           pubDate: new Date(item.pubDate[0]).toUTCString(),
-          media: item["media:content"] ? item["media:content"][0].$.url : null,
-        }));
+          media: media,
+        }});
         resolve(simplifiedItems);
       });
     });
-  } catch (error) {
-    console.error("Error while receiving RSS data:", error);
-    throw { error: "Error while receiving RSS data" };
-  }
 }
